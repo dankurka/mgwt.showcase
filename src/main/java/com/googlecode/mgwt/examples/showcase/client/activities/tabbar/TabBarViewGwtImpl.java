db781292d8cd7927e13eb5dda59c36ac1b541336
Katharina Fahnenbruck <katharina.fahnenbruck@gmail.com>
2012-02-02 00:30:20 +0100

    removed scrolling (tab bar does not have a scroll panel anymore)

--- src/main/java/com/googlecode/mgwt/examples/showcase/client/activities/tabbar/TabBarViewGwtImpl.java
+++ src/main/java/com/googlecode/mgwt/examples/showcase/client/activities/tabbar/TabBarViewGwtImpl.java
@@ -31,17 +31,16 @@ public class TabBarViewGwtImpl extends DetailViewGwtImpl implements TabBarView {
 	public TabBarViewGwtImpl() {
 
 		TabPanel tabPanel = new TabPanel();
-		tabPanel.setScrollingEnabledX(false);
 
 		tabPanel.add(new BookmarkTabBarButton(), new Label("Bookmark"));
-		//		tabPanel.add(new ContactsTabBarButton(), new Label("Contacts"));
-		//		tabPanel.add(new DownloadsTabBarButton(), new Label("Downloads"));
-		//		tabPanel.add(new FavoritesTabBarButton(), new Label("Favorites"));
-		//		tabPanel.add(new FeaturedTabBarButton(), new Label("Featured"));
+		// tabPanel.add(new ContactsTabBarButton(), new Label("Contacts"));
+		// tabPanel.add(new DownloadsTabBarButton(), new Label("Downloads"));
+		// tabPanel.add(new FavoritesTabBarButton(), new Label("Favorites"));
+		// tabPanel.add(new FeaturedTabBarButton(), new Label("Featured"));
 		tabPanel.add(new HistoryTabBarButton(), new Label("History"));
-		//		tabPanel.add(new MoreTabBarButton(), new Label("More"));
-		//		tabPanel.add(new MostRecentTabBarButton(), new Label("Most Recent"));
-		//		tabPanel.add(new MostViewedTabBarButton(), new Label("Most Viewed"));
+		// tabPanel.add(new MoreTabBarButton(), new Label("More"));
+		// tabPanel.add(new MostRecentTabBarButton(), new Label("Most Recent"));
+		// tabPanel.add(new MostViewedTabBarButton(), new Label("Most Viewed"));
 		tabPanel.add(new SearchTabBarButton(), new Label("Search"));
 
 		main.add(tabPanel);
